79a523313de4bf0bda0decd9ff6bb23d8feae1ac
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2024-04-18 00:32:25 +0900

    Feat: router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
+import { Route, Routes } from "react-router-dom"
+import Layout from "./component/layout/Layout"
+import MainPage from "./pages/MainPage"
+import LoginPage from "./pages/LoginPage"
+import SignUpSelectPage from "./pages/SignUpSelectPage"
+import SignUpNormal from "./pages/SignUpNormal"
+import SignUpSeller from "./pages/SignUpSeller"
+
 function App() {
-  
 
   return (
     <>
-      
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<SignUpSelectPage />} />
+          <Route path="/register/user" element={<SignUpNormal />} />
+          <Route path="/register/seller" element={<SignUpSeller />} />
+        </Route>
+      </Routes>
     </>
   )
 }
