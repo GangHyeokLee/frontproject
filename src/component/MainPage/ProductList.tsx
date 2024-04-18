53745fa32bfa656720b7013fae90570ebad094c7
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2024-04-18 12:35:18 +0900

    Rename: product list

---
+++ src/component/MainPage/ProductList.tsx
@@ -0,0 +1,21 @@
+import { Tabs, TabsList } from "@/components/ui/tabs"
+import CategoryItem from "./CategoryItem"
+
+const categories = [
+  'ALL', '상의', '하의', '신발', '모자', '안경'
+]
+
+const ProductList: React.FC<{ setFilter: React.Dispatch<React.SetStateAction<string>> }> = ({ setFilter }) => {
+
+  return (
+    <Tabs>
+      <TabsList className={`mt-5 items-center flex justify-center bg-gray-300 h-fit`}>
+        {categories.map((x) =>
+          <CategoryItem name={x} setFilter={setFilter} />
+        )}
+      </TabsList>
+    </Tabs>
+  )
+}
+
+export default ProductList
