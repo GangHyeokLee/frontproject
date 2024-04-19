6ad9c97360d52e6fe7f3c9c9d99924cb38c406f2
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2024-04-19 16:30:34 +0900

    Fix: firebase connection

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from "path"
+import viteTsconfigPaths from 'vite-tsconfig-paths';
+import envCompatible from 'vite-plugin-env-compatible';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteTsconfigPaths(), envCompatible()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
