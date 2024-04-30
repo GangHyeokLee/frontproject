aa41bdf322356216d7f6a54a04b62b6c80da6acb
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2024-04-30 21:26:17 +0900

    Feat: Receipt type

---
+++ src/types/Receipt.type.ts
@@ -0,0 +1,9 @@
+import { CartProduct } from "./product.type";
+
+export interface Receipt {
+  address: string;
+  createdAt: Date;
+  products: CartProduct[];
+  price: number;
+  message: string;
+}
